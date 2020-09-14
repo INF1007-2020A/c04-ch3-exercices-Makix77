8468cd5b018fd8747ce6b732dfb963271615fdce
Makix77 <olivier2000_9@hotmail.ca>
2020-09-14 23:08:10 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -32,7 +32,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return (temperature - 32)/(9/5)
+    return (temperature - 32)
 
 
 def main() -> None:
