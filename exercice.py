df50b4559d4d4cd7c9695cf8902d1710bc06058a
Makix77 <olivier2000_9@hotmail.ca>
2020-09-14 23:04:51 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     print(angle_mins/60)
     print(angle_secs/3600)
-    return math.radians(angle_degs + (angle_mins + angle_secs/60)/60)
+    return math.radians(angle_degs + angle
 
 
 def to_degrees(angle_rads: float) -> tuple:
