48f72566ea1a66302ee3699b6aa11b3426f4ad87
Makix77 <olivier2000_9@hotmail.ca>
2020-09-14 22:53:51 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,31 +5,31 @@
 import math
 
 def square_root(a: float) -> float:
-    return 0.0
+    return math.sqrt(a)
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a*a
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a+b+c)/3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return math.radians(angle_degs + (angle_mins + angle_secs/60)/60)
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    return math.degrees(angle_rads) - int(math.degrees(angle_rads)), 
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature * 9/5) + 32
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) * 5/9
 
 
 def main() -> None:
